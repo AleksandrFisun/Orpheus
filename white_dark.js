f8f840816293397888dc6608bf6552d533131351
AleksandrFisun <aleksgoldman1996@gmail.com>
2022-06-12 01:20:39 +0300

    1

---
+++ white_dark.js
@@ -0,0 +1,10 @@
+let switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme");
+    if (theme.getAttribute("href") ==   "dark.css") {
+        theme.href = "white.css";   
+    } else {
+        theme.href = "dark.css";
+    }
+}
